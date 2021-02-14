f112a2f987f083cf4ab72a9cab41574db7fed577
Ronald S. Picard <rpicard92@gmail.com>
2021-02-14 11:18:12 -0500

    Add project files.

---
+++ Cpp-Chess-Engine/Cpp-Chess-Engine.cpp
@@ -0,0 +1,12 @@
+﻿// Cpp-Chess-Engine.cpp : Defines the entry point for the application.
+//
+
+#include "Cpp-Chess-Engine.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
