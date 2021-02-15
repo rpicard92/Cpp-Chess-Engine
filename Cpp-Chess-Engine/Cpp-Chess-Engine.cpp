13f557c6dec5f2bd66b7f61ba6329053fa51c21d
Ronald S. Picard <rpicard92@gmail.com>
2021-02-15 16:59:14 -0500

    updates: working, but I suspect somthing is wrong with the valid moves since at a depth of 5 it finishes fast.

--- Cpp-Chess-Engine/Cpp-Chess-Engine.cpp
+++ Cpp-Chess-Engine/Cpp-Chess-Engine.cpp
@@ -9,8 +9,8 @@ int main()
 {
 	Model model = Model();
 	Solver solver = Solver();
-	int test = 1;
-	std::pair<Model, int> pair = solver.solve(model, test, 'w');
+	int searchDepth = 5;
+	std::pair<Model, int> pair = solver.solve(model, searchDepth, 'w');
 	cout << "Score: " + std::to_string(pair.second) << endl;
 	return 0;
 }
